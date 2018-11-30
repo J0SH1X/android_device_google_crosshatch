c100b59049f9c7a419d9629de2f62fd7cb6decb2
J0SH1X <aljoshua.hell@gmail.com>
2018-11-30 17:11:39 +0100

    prebuild vendor and disabled vbmeta

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -5,3 +5,10 @@ TARGET_COMPILE_WITH_MSM_KERNEL := true
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_SOURCE := kernel/google/crosshatch
 TARGET_NEEDS_DTBOIMAGE := true
+
+#Prebuild vendor
+BOARD_PREBUILT_VENDORIMAGE := device/google/crosshatch/vendor.img
+
+#Disable VBMETA
+
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flag 2
